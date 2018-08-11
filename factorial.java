e7dbe7a728b959735337985c48768e43ee151641
aneesha22 <42198319+aneesha22@users.noreply.github.com>
2018-08-11 12:24:00 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+                int num = 20;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
